c9e61df38456028b4e73da0c6efff5d7142ab174
David Martin Rius <david.martin@proferox.com>
2024-02-26 17:54:44 +0100

    Refactor code + setup.py + fix requirements.txt + .gitihnore + bugfix when saving audio file path in .csv

---
+++ speech_dataset_generator/audio_manager/audio_manager.py
@@ -0,0 +1,207 @@
+import numpy as np
+import torch
+import gc 
+import os
+
+import speechmetrics #https://github.com/aliutkus/speechmetrics
+from unsilence import Unsilence
+
+#https://github.com/ina-foss/inaSpeechSegmenter # sudo apt-get install ffmpeg
+from df.enhance import enhance, init_df, load_audio, save_audio
+
+import torchaudio
+from resemble_enhance.enhancer.inference import denoise, enhance as resemble_enhancer
+from scipy.io.wavfile import write
+
+from speech_dataset_generator.utils.utils import get_device
+
+class AudioManager:
+    
+    def __init__(self):
+        window_length = 30 # seconds
+        self.metrics = speechmetrics.load('absolute', window_length)
+        
+    def process(self, input_audio, output_directory, filter_types):
+        
+        if "None" not in filter_types:
+            output_audio_file = self.get_output_file_name(input_audio, output_directory)
+            self.enhance_audio(input_audio, output_audio_file, filter_types)
+        else:
+            output_audio_file = input_audio
+        
+        if not self.has_speech_quality(output_audio_file):
+            return None
+        
+        return output_audio_file
+    
+    def get_output_file_name(self, input_audio, output_directory):
+        # Extract the input file name without extension
+        file_name_without_extension, extension = os.path.splitext(os.path.basename(input_audio))
+
+        # Create the output file name by adding "_enhanced" suffix
+        output_file_name = f"{file_name_without_extension}_enhanced{extension}"
+        
+        enhanced_directory = os.path.join(output_directory, "enhanced")
+
+        if not os.path.exists(enhanced_directory):
+            os.makedirs(enhanced_directory)
+            
+        return os.path.join(enhanced_directory, output_file_name)
+
+    #https://github.com/Rikorose/DeepFilterNet
+    #alternatives to deepfilternet 
+    #https://github.com/resemble-ai/resemble-enhance
+    #https://github.com/shahules786/mayavoz 
+    def enhance_audio(self, noisy_audio, output_audio_file, types=["deepfilternet"]):
+            
+        temp_output = noisy_audio  # Initial audio loading
+
+        for enhancement_type in types:
+            if enhancement_type == "deepfilternet":
+                temp_output = self.enhance_audio_deepfilternet(temp_output, output_audio_file)
+            elif enhancement_type == "enhance_audio_resembleai":
+                temp_output = self.enhance_audio_resembleai(temp_output, output_audio_file)
+            elif enhancement_type == "mayavoz":
+                temp_output = self.enhance_audio_mayavoz(temp_output, output_audio_file)
+        
+        self.remove_sliences(temp_output)
+
+    def enhance_audio_deepfilternet(self, noisy_audio, output_audio_file):
+
+        model, df_state, _ = init_df()  # Load default model
+
+        audio, info = load_audio(noisy_audio, sr=df_state.sr())
+        
+        minutes = 1 # it is easy to edit time in minutes than seconds
+        seconds = minutes * 60
+
+        # Split audio into 5min chunks
+        audio_chunks = [audio[:, i:i + seconds * info.sample_rate]
+                        for i in range(0, audio.shape[1], seconds * info.sample_rate)]
+
+        enhanced_chunks = []
+        for ac in audio_chunks:
+            enhanced_chunks.append(enhance(model, df_state, ac))
+            torch.cuda.empty_cache()
+            gc.collect()
+
+        enhanced = torch.cat(enhanced_chunks, dim=1)
+
+        assert enhanced.shape == audio.shape, 'Enhanced audio shape does not match original audio shape.'
+
+        save_audio(output_audio_file, enhanced, sr=df_state.sr())  
+        
+        del model, df_state, audio, info, enhanced_chunks, enhanced
+        torch.cuda.empty_cache()
+        gc.collect()
+        
+        return output_audio_file
+       
+    def split_audio_into_chunks(self, audio, chunk_size):
+        """
+        Split audio into chunks of specified size.
+
+        Parameters:
+            audio (np.ndarray): The audio data.
+            chunk_size (int): Size of each chunk in samples.
+
+        Returns:
+            List[np.ndarray]: List of audio chunks.
+        """
+        num_samples = len(audio)
+        num_chunks = num_samples // chunk_size
+        chunks = [audio[i * chunk_size: (i + 1) * chunk_size] for i in range(num_chunks)]
+        return chunks
+    
+    def enhance_audio_resembleai(self, noisy_audio, output_audio_file):
+        
+        solver = "midpoint" #rk4, euler, midpoint
+        denoising = True
+        nfe = 128
+        tau = 0
+        chunk_duration = 20  # 1 minute in seconds
+
+        if noisy_audio is None:
+            return None, None
+
+        solver = solver.lower()
+        nfe = int(nfe)
+        lambd = 0.9 if denoising else 0.1
+
+        # Load the entire audio file
+        dwav, sr = torchaudio.load(noisy_audio)
+        dwav = dwav.mean(dim=0)
+
+        device = get_device()
+        
+        # Calculate chunk size based on duration
+        chunk_size = int(sr * chunk_duration)
+        
+        # Split the audio into chunks
+        audio_chunks = self.split_audio_into_chunks(dwav.cpu().numpy(), chunk_size)
+
+        enhanced_chunks = []
+
+        for chunk in audio_chunks:
+            
+            chunk_tensor = torch.tensor(chunk)
+
+            # Apply enhancement to each chunk
+            wav2_chunk, new_sr = resemble_enhancer(chunk_tensor, sr, device, nfe=nfe, solver=solver, lambd=lambd, tau=tau)
+            
+            # Save the enhanced chunk to the list
+            enhanced_chunks.append(wav2_chunk)
+
+        # Concatenate all enhanced chunks
+        enhanced_audio = np.concatenate(enhanced_chunks)
+
+        # Write the concatenated enhanced audio to the output file
+        write(output_audio_file, new_sr, enhanced_audio)
+
+        return output_audio_file
+        
+    def enhance_audio_mayavoz(self, noisy_audio, output_audio_file):
+        #TODO
+        return output_audio_file
+        
+    # https://github.com/lagmoellertim/unsilence
+    def remove_sliences(self, path_to_audio_file):
+        
+        u = Unsilence(path_to_audio_file)
+        
+        u.detect_silence()
+        
+        #rewrite the file with no silences
+        u.render_media(path_to_audio_file, audio_only=True)  # Audio only specified
+        
+        del u
+        
+    #https://github.com/aliutkus/speechmetrics
+    def has_speech_quality(self, path_to_audio_file):
+
+        scores = self.metrics(path_to_audio_file)
+
+        average_scores = {}
+
+        for metric_name, scores_array in scores.items():
+            # Calculate the average of the array/list
+            average_score = np.mean(scores_array)
+
+            # Print the result
+            #print(f"Average {metric_name} score: {average_score}")
+            
+            average_scores[metric_name] = average_score
+            
+        if average_scores['mosnet'] >= 3:
+            return True
+
+        return False
+    
+    def has_music(self, segmentation):
+
+        labels = [item[0] for item in segmentation if item[0] in ('music')]
+
+        if 'music' in labels:
+            return True
+
+        return False
---
+++ speech_dataset_generator/audio_processor/audio_processor.py
@@ -0,0 +1,51 @@
+import os
+import yt_dlp
+import chromadb
+
+from speech_dataset_generator.dataset_generator.dataset_generator import DatasetGenerator
+
+    
+def get_local_audio_files(input_folder):
+    all_files = os.listdir(input_folder)
+    return [os.path.join(input_folder, file) for file in all_files if file.lower().endswith(('.mp3', '.wav', '.flac', '.ogg', '.aac', '.wma'))]
+
+def get_youtube_audio_files(urls, output_directory):
+    
+    downloaded_files = []
+    if not urls:
+        return downloaded_files
+    
+    youtube_files_output_directory = os.path.join(output_directory, "youtube_files") 
+    
+    if not os.path.exists(youtube_files_output_directory):
+        os.makedirs(youtube_files_output_directory)
+        
+    audio_format = "wav"
+    
+    for url in urls:
+        output_template = os.path.join(youtube_files_output_directory, f"%(title)s.{audio_format}")
+            
+        ydl_opts = {
+            'format': 'bestaudio/best',
+            'extractaudio': True,
+            'audioformat': 'wav',
+            'outtmpl': output_template,
+        }
+
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
+            
+    downloaded_files = [os.path.join(youtube_files_output_directory, file_name) for file_name in os.listdir(youtube_files_output_directory)]
+
+    return downloaded_files
+
+def process_audio_files(audio_files, output_directory, start, end, filter_types):
+    
+    dataset_generator = DatasetGenerator()
+    
+    client = chromadb.PersistentClient(path=os.path.join(output_directory, "chroma_database"))
+    collection = client.get_or_create_collection(name="speakers")
+
+    for audio_file in audio_files:
+        print("Processing:", audio_file)
+        dataset_generator.process(audio_file, output_directory, start, end, filter_types, collection)
